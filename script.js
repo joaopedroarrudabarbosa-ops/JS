c805f2e5fbbd23c30ae731778100594765621c56
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-01 20:55:10 -0300

    teste

---
+++ script.js
@@ -0,0 +1,16 @@
+// alert("Hello World!"); // printa na tela
+// prompt("Qual é o seu nome?"); // aparece na tela uma caixa para responder qlqr coisa
+// confirm("Você deseja sair?"); // aparece na tela para confirmar 
+// console.log("EITAAAAAAAAAAAAA"); // faz aparecer algo no console
+// let nome = "João"; // valor que pode variar
+// const pi = 3.145; // valor fixo
+// alert(nome);
+// console.log(1/0); // infinity
+// console.log(1/"a"); // NaN - invalido
+// null - vazio intencional
+// undefined - valor indefinido 
+// typeof e uma variavel na frente traz o tipo da variavel (string, number etc)
+
+let nome = prompt("Qual é o seu nome?");
+let idade = prompt("Qual sua idade?");
+alert(`Seu nome é ${nome} e sua idade é ${idade}`);
