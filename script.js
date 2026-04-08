69306fd015c6fb05a2dce4a2d4f4ad94a7c334f1
Joao Pedro <joaopedroarrudabarbosa@gmail.com>
2026-04-08 18:55:02 -0300

    teste

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-// alert("Hello World!"); // printa na tela
-// prompt("Qual é o seu nome?"); // aparece na tela uma caixa para responder qlqr coisa
-// confirm("Você deseja sair?"); // aparece na tela para confirmar 
-// console.log("EITAAAAAAAAAAAAA"); // faz aparecer algo no console
-// let nome = "João"; // valor que pode variar
-// const pi = 3.145; // valor fixo
+// alert("Hello World!"); - // printa na tela
+// prompt("Qual é o seu nome?"); - // aparece na tela uma caixa para responder qlqr coisa
+// confirm("Você deseja sair?"); - // aparece na tela para confirmar 
+// console.log("EITAAAAAAAAAAAAA"); - // faz aparecer algo no console
+// let nome = "João"; - // valor que pode variar
+// const pi = 3.145; - // valor fixo
 // alert(nome);
-// console.log(1/0); // infinity
-// console.log(1/"a"); // NaN - invalido
+// console.log(1/0); - // infinity
+// console.log(1/"a"); - // NaN - invalido
 // null - vazio intencional
 // undefined - valor indefinido 
-// typeof e uma variavel na frente traz o tipo da variavel (string, number etc)
+// -typeof-  e uma variavel na frente traz o tipo da variavel (string, number etc)
 
 let nome = prompt("Qual é o seu nome?");
 let idade = prompt("Qual sua idade?");
