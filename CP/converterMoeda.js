cbdfc41bb6de9c408cf8cb7c2cbe7fdb31839b1d
Joao Pedro <joaopedroarrudabarbosa@gmail.com>
2026-05-13 18:41:43 -0300

    CP

--- CP/converterMoeda.js
+++ CP/converterMoeda.js
@@ -3,11 +3,24 @@ function converterMoeda(valor, moedaAtual, moeda = "USD") {
         return "Valor inválido."
     }
     if (moedaAtual !== "BRL") {
+        return "Moeda de origem inválida."
+    }
+    if (moeda !== "USD" && moeda !== "EUR" && moeda !== "GBP") {
         return "Moeda de destino inválida."
     }
-    const taxas = {
-        "USD": 5.70,
-        "EUR": 6.20,
-        "GBP": 7.10
+    else if (moeda === "USD" || moeda === "EUR" || moeda === "GBP") {
+        const taxas = {
+            "USD": 5.70,
+            "EUR": 6.20,
+            "GBP": 7.10
+        }
+        const resultado = valor / taxas[moeda]
+        return `${valor} = ${resultado.toFixed(2)} ${moeda}`
     }
 }
+
+let valor = parseFloat(prompt("Digite o valor em reais: "))
+let moedaAtual = prompt("Digite a moeda atual (BRL): ")
+let moedaDestino = prompt("Digite a moeda de destino (USD, EUR ou GBP):")
+
+alert(converterMoeda(valor, moedaAtual, moedaDestino))
